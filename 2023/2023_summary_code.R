08e3f2e5507ec0376c98a2890d1a46a2c11adad3
harley-zhang <harleyzgolego@gmail.com>
2024-03-12 20:31:22 -0400

    small fixes

--- 2023/2023_summary_code.R
+++ 2023/2023_summary_code.R
@@ -25,22 +25,24 @@ average_height <- input_data %>%
 
 # Step 5: Check for regeneration presence
 regeneration_presence <- input_data %>%
-  filter(!is.na(size_class)) %>%
+  filter(alive_or_dead == "living", !is.na(size_class)) %>%
   group_by(new_plot_key) %>%
-  summarise(regeneration_presence = ifelse(any(size_class == "sapling" | size_class == "seedling"), "Regeneration present", "Regeneration absent"))
+  summarise(regeneration_presence = ifelse(any(size_class == "sapling" | size_class == "seedling"), "Regeneration present", "No regeneration"))
 
 # Step 6: Check for insect presence
-insect_damage_present <- input_data %>%
+insect_damage_presence <- input_data %>%
+  filter(!is.na(insect_presence)) %>%
   group_by(new_plot_key) %>%
-  summarise(insect_damage_presence = ifelse(any(insect_presence == 1), "Insect damage present", "Insect damage absent"))
+  summarise(insect_damage_presence = ifelse(any(insect_presence == 1), "Insect damage present", "No insect damage"))
 
 # Step 7: Check for browse presence
-browse_damage_present <- input_data %>%
+browse_damage_presence <- input_data %>%
+  filter(!is.na(browse_presence)) %>%
   group_by(new_plot_key) %>%
-  summarise(browse_damage_presence = ifelse(any(browse_presence == 1), "Browse present", "Browse absent"))
+  summarise(browse_damage_presence = ifelse(any(browse_presence == 1), "Browse present", "No browse present"))
 
 # Step 8: Find the dominant tree species for each plot
-dominant_species_tree <- input_data %>%
+dominant_tree_species <- input_data %>%
   filter(alive_or_dead == "living", size_class == "tree", !is.na(tree_species)) %>%
   group_by(new_plot_key) %>%
   summarise(dominant_tree_species = {
@@ -82,10 +84,10 @@ dominant_species_tree <- input_data %>%
 
 # Merge all outputs into one dataframe
 output_statistics <- Reduce(function(x, y) merge(x, y, by = "new_plot_key", all = TRUE), 
-                            list(output_statistics_basal, output_statistics_dbh, 
-                                 output_statistics_height, output_statistics_regeneration, 
-                                 output_statistics_insect, output_statistics_browse, 
-                                 output_statistics_species))
+                            list(basal_area, average_dbh, 
+                                 average_height, regeneration_presence, 
+                                 insect_damage_presence, browse_damage_presence, 
+                                 dominant_tree_species))
 
 # Write the output to CSV
 write.csv(output_statistics, file = "/Users/harley/Documents/output_statistics.csv", row.names = FALSE)
