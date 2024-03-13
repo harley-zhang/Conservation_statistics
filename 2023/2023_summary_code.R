70ca3f11d02c4423cf0043d921a6968086b60fa2
harley-zhang <78547210+harleyhzhang@users.noreply.github.com>
2024-03-12 20:05:00 -0400

    small updates

--- 2023/2023_summary_code.R
+++ 2023/2023_summary_code.R
@@ -6,41 +6,41 @@ library(tidyr)
 input_data <- read.csv("/Users/harley/Documents/Trinchera_summary/2023/cleanedMerged_Forestry23_030524_2.csv")
 
 # Step 2: Calculate basal area per acre
-output_statistics_basal <- input_data %>%
+basal_area <- input_data %>%
   filter(alive_or_dead == "living", !is.na(dbh_cm_tree)) %>%
   group_by(new_plot_key) %>%
   summarise(basal_area_per_acre_in = round(sum(0.005454 * (dbh_cm_tree/2.54)^2) * 5, 2))
 
 # Step 3: Calculate average dbh in inches
-output_statistics_dbh <- input_data %>%
+average_dbh <- input_data %>%
   filter(alive_or_dead == "living", !is.na(dbh_cm_tree)) %>%
   group_by(new_plot_key) %>%
   summarise(average_dbh_in = round(mean(dbh_cm_tree) / 2.54, 2))
 
 # Step 4: Calculate average height in feet
-output_statistics_height <- input_data %>%
+average_height <- input_data %>%
   filter(alive_or_dead == "living", !is.na(total_height_m_tree)) %>%
   group_by(new_plot_key) %>%
   summarise(average_height_ft = round(mean(total_height_m_tree) * 3.28084, 2))
 
 # Step 5: Check for regeneration presence
-output_statistics_regeneration <- input_data %>%
+regeneration_presence <- input_data %>%
   filter(!is.na(size_class)) %>%
   group_by(new_plot_key) %>%
-  summarise(regeneration_present = ifelse(any(size_class == "sapling" | size_class == "seedling"), "Regeneration present", "Regeneration absent"))
+  summarise(regeneration_presence = ifelse(any(size_class == "sapling" | size_class == "seedling"), "Regeneration present", "Regeneration absent"))
 
 # Step 6: Check for insect presence
-output_statistics_insect <- input_data %>%
+insect_damage_present <- input_data %>%
   group_by(new_plot_key) %>%
-  summarise(insect_present = ifelse(any(insect_presence == 1), "Insect damage present", "Insect damage absent"))
+  summarise(insect_damage_presence = ifelse(any(insect_presence == 1), "Insect damage present", "Insect damage absent"))
 
 # Step 7: Check for browse presence
-output_statistics_browse <- input_data %>%
+browse_damage_present <- input_data %>%
   group_by(new_plot_key) %>%
-  summarise(browse_present = ifelse(any(browse_presence == 1), "Browse present", "Browse absent"))
+  summarise(browse_damage_presence = ifelse(any(browse_presence == 1), "Browse present", "Browse absent"))
 
 # Step 8: Find the dominant tree species for each plot
-output_statistics_species <- input_data %>%
+dominant_species_tree <- input_data %>%
   filter(alive_or_dead == "living", size_class == "tree", !is.na(tree_species)) %>%
   group_by(new_plot_key) %>%
   summarise(dominant_tree_species = {
