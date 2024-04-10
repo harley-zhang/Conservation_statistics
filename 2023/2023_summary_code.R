1486cd804f3fe18fc8842206718cdea64d4ca833
harley-zhang <78547210+harleyhzhang@users.noreply.github.com>
2024-04-10 09:46:49 -0400

    change output stats file saving path to 2022/2023 respectively

--- 2023/2023_summary_code.R
+++ 2023/2023_summary_code.R
@@ -172,7 +172,7 @@ output_statistics_2023 <- Reduce(function(x, y) merge(x, y, by = "new_plot_key",
                                  insect_damage_presence, browse_damage_presence, list_damage))
 
 # Write output to CSV
-write.csv(output_statistics, file = "/Users/harley/Documents/2023_output_statistics.csv", row.names = FALSE)
+write.csv(output_statistics, file = "/Users/harley/Documents/Github/Trinchera_summary/2023/2023_output_statistics.csv", row.names = FALSE)
 
 
 
