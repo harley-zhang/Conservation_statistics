eae3d1a4f51193eabac4ef26fafce5048af617d6
harley-zhang <78547210+harleyhzhang@users.noreply.github.com>
2024-04-14 10:11:16 -0400

    as.character() to both dataframes in stand, merge

--- stand_stats/stand_summary.R
+++ stand_stats/stand_summary.R
@@ -3,10 +3,16 @@ library(dplyr)
 library(tidyr)
 
 # Step 1: Read in raw/input CSVs
-output_summary_2022 <- read.csv("/Users/harley/Documents/Github/Trinchera_summary/plot_stats/2022/2022_output_summary.csv")
-output_summary_2023 <- read.csv("/Users/harley/Documents/Github/Trinchera_summary/plot_stats/2023/2023_output_summary.csv")
+summary_2022 <- read.csv("/Users/harley/Documents/Github/Trinchera_summary/plot_stats/2022/2022_summary.csv")
+summary_2023 <- read.csv("/Users/harley/Documents/Github/Trinchera_summary/plot_stats/2023/2023_summary.csv")
 
-# Step 2: 
+# Step 2: Convert dataframes all to <character>
+summary_2022 <- mutate_all(summary_2022, as.character)
+summary_2023 <- mutate_all(summary_2023, as.character)
+
+# Step 3: Merge dataframes
+summary_merged <- bind_rows(summary_2022, summary_2023)
+
 
 
 # Step XX: Write output to CSV
